d03ac314a80f0235080a55bd1a785b9d7936a307
dearcharlyn <villavicenciocharlyn@gmail.com>
2021-06-24 16:59:36 +0800

    Allowed Hosts

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i-0bx!i%50ew4=kq!vfmt+icvhsi2!nb-1z36&kc!iefrw=qn7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
